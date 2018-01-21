4959e7fe81673dff4da1a9b4e49247648e11e5ea
alwaysFrank <1798872204@qq.com>
2018-01-22 00:28:19 +0800

    regular expression Capital first element of word

--- learning/RegularExpression.py
+++ learning/RegularExpression.py
@@ -30,7 +30,7 @@ pt = re.compile(r'(\w+)(\w*)')
 
 greeting = 'i say, hello world!'
 print(pt.match(greeting).groups())
-print(pt.sub(r'2 1',greeting))
+print(pt.sub(r'w blank',greeting))
 
 def func(m):
     return m.group(1).title() + '' +m.group(2).title()
